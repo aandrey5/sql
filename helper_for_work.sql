95f7eb30033a8237378e132e7f99561766608506
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-04-28 12:52:22 +0300

    Create helper_for_work.sql

---
+++ helper_for_work.sql
@@ -0,0 +1,11 @@
+___________________________________
+--------PostgreSQL myHELPER--------
+___________________________________
+
+-- copy table all
+
+create table sales_db_copy as
+  select * from sales_db;
+  
+  
+  
