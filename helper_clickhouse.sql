0019a3eccade6f72137b73575c81d365d215cd42
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-04-28 23:59:48 +0300

    Update helper_clickhouse.sql

--- helper_clickhouse.sql
+++ helper_clickhouse.sql
@@ -6,3 +6,19 @@ sudo clickhouse start
 
 clickhouse-client
 
+---------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
+-- When executing command $ sudo reboot. When OS boot tries to connect to server CH. But get the error:
+
+$ clickhouse-client
+ClickHouse client version 1.1.54385.
+Connecting to localhost:9000.
+Code: 210. DB::NetException: Connection refused: (localhost:9000, ::1)
+OS: ubuntu 16.04
+clean install
+<listen_host>::</listen_host> - is work...
+log:
+
+IF YOU UNCOMMENT AND COMMENT LINE AFTER, BE SHURE TO SET --> IN END !!!
+---------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
