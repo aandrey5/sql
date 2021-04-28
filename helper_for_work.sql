1c776a603b852c5b3f209d97d2b91c2b9f6d74dc
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-04-28 14:51:17 +0300

    Update helper_for_work.sql

--- helper_for_work.sql
+++ helper_for_work.sql
@@ -45,6 +45,67 @@ set
 	margin_percent = replace(margin_percent, ',', '.');
 	
 commit;
+
+
+
+-- change data types in column from var char to double precision
+
+alter table sales_db_copy
+alter column quant_box type double precision
+USING quant_box::double precision;
+
+
+
+-- multiple altering columns
+
+begin;
+
+alter table sales_db_copy
+
+alter column rc_grotex_nds type double precision
+USING rc_grotex_nds::double precision,
+
+
+alter column price_ru_nds type double precision
+USING price_ru_nds::double precision,
+
+alter column deviation_rc type varchar(20)
+USING deviation_rc::varchar(20),
+
+alter column quant_box type double precision
+USING quant_box::double precision,
+
+alter column sum_price_grtx_nds type double precision
+USING sum_price_grtx_nds::double precision,
+
+alter column quant_box type double precision
+USING quant_box::double precision,
+
+alter column sum_price_grtx_no_nds type double precision
+USING sum_price_grtx_no_nds::double precision,
+
+alter column sum_purchase_nds type double precision
+USING sum_purchase_nds::double precision,
+
+alter column sum_purchase_no_nds type double precision
+USING sum_purchase_no_nds::double precision,
+
+alter column sum_sale_nds type double precision
+USING sum_sale_nds::double precision,
+
+alter column sum_sale_no_nds type double precision
+USING sum_sale_no_nds::double precision,
+
+alter column sumdiscount type double precision
+USING sumdiscount::double precision,
+
+alter column sum_vat type double precision
+USING sum_vat::double precision,
+
+alter column margin_percent type varchar(20)
+USING margin_percent::varchar(20);
+
+commit;
   
   
   
