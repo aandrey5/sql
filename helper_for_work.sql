afedfd26ad2c51462b76b9414ffbbb29002782a6
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-04-28 13:50:17 +0300

    Update helper_for_work.sql

--- helper_for_work.sql
+++ helper_for_work.sql
@@ -15,6 +15,36 @@ with no data
 
 -- The create table like syntax will include all triggers, indexes, constraints, etc. But not include data.
 create table my_table_copy (like my_table including all)
+
+
+-- update in transaction
+begin;
+
+update 
+	sales_db_copy
+set price_grotex_no_nds = replace(price_grotex_no_nds, ',', '.');
+
+
+
+-- safety replace / update
+begin;
+update 
+	sales_db_copy
+set 
+	price_ru_nds = replace(price_ru_nds, ',', '.'),
+	rc_grotex_nds = replace(rc_grotex_nds, ',', '.'),
+	deviation_rc = replace(deviation_rc, ',', '.'),
+	sum_price_grtx_nds = replace(sum_price_grtx_nds, ',', '.'),
+	sum_price_grtx_no_nds = replace(sum_price_grtx_no_nds, ',', '.'),
+	sum_purchase_nds = replace(sum_purchase_nds, ',', '.'),
+	sum_purchase_no_nds = replace(sum_purchase_no_nds, ',', '.'),
+	sum_sale_nds = replace(sum_sale_nds, ',', '.'),
+	sum_sale_no_nds = replace(sum_sale_no_nds	, ',', '.'),
+	sumdiscount = replace(sumdiscount, ',', '.'),
+	sum_vat = replace(sum_vat, ',', '.'),
+	margin_percent = replace(margin_percent, ',', '.');
+	
+commit;
   
   
   
