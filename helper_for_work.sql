85fa14b1e1bf9b9ccd1cbe9c5e1ad727faf02b0e
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-04-28 12:54:36 +0300

    Update helper_for_work.sql

--- helper_for_work.sql
+++ helper_for_work.sql
@@ -8,4 +8,13 @@ create table sales_db_copy as
   select * from sales_db;
   
   
+ -- This will create the table with all the data, but without indexes and triggers etc.
+ create table my_table_copy as
+  select * from my_table
+with no data
+
+-- The create table like syntax will include all triggers, indexes, constraints, etc. But not include data.
+create table my_table_copy (like my_table including all)
+  
+  
   
