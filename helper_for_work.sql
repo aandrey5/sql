43217e0f701e32fba0327b05c0cd99841e662234
aandrey5 <60394118+aandrey5@users.noreply.github.com>
2021-05-18 13:04:21 +0300

    Update helper_for_work.sql

--- helper_for_work.sql
+++ helper_for_work.sql
@@ -172,3 +172,15 @@ WHERE
 delete from copy001
 where "year" = '2021' and "month" = 'Март';
 							
+							
+-- size of all tables in database
+
+\c dwh
+							
+SELECT nspname || '.' || relname AS "relation",
+    pg_size_pretty(pg_relation_size(C.oid)) AS "size"
+  FROM pg_class C
+  LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+  ORDER BY pg_relation_size(C.oid) DESC;							
+							
