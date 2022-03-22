95cc9c8a636228273c1c1035cba2ff837e52df69
Andrey Morozov <60394118+aandrey5@users.noreply.github.com>
2022-03-22 11:44:21 +0300

    Update advanced_postgresql_queries.sql

--- advanced_postgresql_queries.sql
+++ advanced_postgresql_queries.sql
@@ -503,3 +503,32 @@ group by rollup (category_id);
 
 
 
+--==========================================================================
+--==========================================================================
+-- RANKING FUNC
+--==========================================================================
+--==========================================================================
+
+
+-- ORDER BY
+
+select 
+	name,
+	height_inches,
+	rank() over(order by height_inches desc),
+	dense_rank() over(order by height_inches desc)
+from public.people_heights
+order by height_inches  desc;
+
+
+-- PARTITION BY ORDER by 
+
+select 
+	name,
+	height_inches,
+	gender,
+	rank() over(partition by gender order by height_inches desc),
+	dense_rank() over(partition by gender order by height_inches desc)
+from public.people_heights
+order by gender, height_inches  desc;
+
