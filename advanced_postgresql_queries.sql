ac33a8d10fc8d09fba29240ef3321b82f5acc0de
Andrey Morozov <60394118+aandrey5@users.noreply.github.com>
2022-03-22 17:09:50 +0300

    Update advanced_postgresql_queries.sql

--- advanced_postgresql_queries.sql
+++ advanced_postgresql_queries.sql
@@ -606,3 +606,15 @@ from inventory.products
 order by category_id, size;
 
 
+-- SOLUTION
+
+select 
+	product_name,
+	category_id ,
+	size ,
+	price,
+	dense_rank() over(order by price desc) as  "rank overall",
+	dense_rank() over(partition by category_id order by price desc) as "rank category",
+	dense_rank() over(partition by size order by price desc) as "rank size"
+from inventory.products
+order by category_id, price desc;
